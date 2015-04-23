ab51ad24d2d94bbab238250a4ea8fead935fbcd9
Patrick Bogdan <patrick.bgdn@gmail.com>
2015-04-23 15:03:01 +0200

    [ex1] now removing stopwords from list and storing total word count for calculations

--- ex1/texteinlesen.py
+++ ex1/texteinlesen.py
@@ -4,6 +4,8 @@
 #textEingabe=open('ebooks/Ten_Acres_Enough_by_Edmund_Morris.txt').read()
 #textEingabe=open('stopwords/german').read()
 textEingabe=open('ebooks/Die_Geschwister_Ein_Schauspiel_in_einem_Akt_by_Johann_Wolfgang_von_Goethe.txt').read()
+stopWordsDoc = ''
+stopWordsDoc = open('stopwords/german').read()
 
 def count_words(text):
   #cleaning up special characters
@@ -14,18 +16,29 @@ def count_words(text):
 
   #creating array of words
   words = text.split(" ")
-
+  
   #removing empty words
   while True:
     if "" in words: words.remove("")
     else: break
+
+  #storing total word count
+  word_count = len(words)
+
+  #removing stop words
+  if stopWordsDoc:
+    stopWords = stopWordsDoc.split("\n")
+    for stopWord in stopWords:
+      if stopWord in words: words.remove(stopWord)
 
   result = []
   distinct_words = []
   #counting words
   for word in words:
     if not word in distinct_words:
-      result.append(word + " : " + str(words.count(word)) + " (" + str(float(words.count(word)) / float(len(words))) + "%)")
+      abs_count = words.count(word)
+      rel_count = float(words.count(word)) / float(word_count)
+      result.append(word + " : " + str(abs_count) + " (" + str(rel_count) + "%)")
       distinct_words.append(word)
 
   #displaying results
