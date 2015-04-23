a15dc9bfc2f66d0082503af4e31304d86b353e1a
Christian_Borg-Krebs <christian.borg-krebs@ixlog.com>
2015-04-23 13:43:45 +0200

    [ex1] first testcase

--- ex1/texteinlesen.py
+++ ex1/texteinlesen.py
@@ -1,40 +1,41 @@
 #!/usr/bin/python
 
-textEingabe=open("einlesen.txt", "r")  //.read() liest ganzen text
-
+textEingabe=open('ebooks/Die_Geschwister_Ein_Schauspiel_in_einem_Akt_by_Johann_Wolfgang_von_Goethe.txt', "r")  #.read() liest ganzen text
+print(textEingabe)
 textEingabe.close()
 
-textAusgabe=open("ausgeben.txt","w")
-textAusgabe.close()
+# textAusgabe=open("ausgeben.txt","w")
 
-Wörter auftrennen 
-textEingabe.split(" ")
+# textAusgabe.close()
 
-Durchgehen
-for wort in textEingabe:
-	if wort
+# Wörter auftrennen 
+# textEingabe.split(" ")
 
-
-Beispiel
+# Durchgehen
+# for wort in textEingabe:
+	# if wort
 
 
-print(count_words("""Fischers Fritz fischt frische Fische, frische Fische fischt Fischers Fritz.
-eins; zwei, zwei; drei, drei, drei."""))
+# Beispiel
 
-def count_words(text):
-    """gibt aus, wie oft jedes Wort im String 'text' vorkommt"""
-    for i in ["'", ",", ".", ";", ":", "?", "!", "(", ")"]:
-        text = text.lower().replace(i, "")
-    text = text.replace("\n", " ")
-    words = text.split(" ")
-    while True:
-        if "" in words: words.remove("")
-        else: break
-    result = []
-    counted_words = []
-    for i in words:
-        if not i in counted_words:
-            result.append(i+" : "+str(words.count(i)))
-            counted_words.append(i)
-    return "\n".join(result)
+
+# print(count_words("""Fischers Fritz fischt frische Fische, frische Fische fischt Fischers Fritz.
+# eins; zwei, zwei; drei, drei, drei."""))
+
+# def count_words(text):
+    # """gibt aus, wie oft jedes Wort im String 'text' vorkommt"""
+    # for i in ["'", ",", ".", ";", ":", "?", "!", "(", ")"]:
+        # text = text.lower().replace(i, "")
+    # text = text.replace("\n", " ")
+    # words = text.split(" ")
+    # while True:
+        # if "" in words: words.remove("")
+        # else: break
+    # result = []
+    # counted_words = []
+    # for i in words:
+        # if not i in counted_words:
+            # result.append(i+" : "+str(words.count(i)))
+            # counted_words.append(i)
+    # return "\n".join(result)
  
