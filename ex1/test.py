f9618feb09aeb8ce5dcffd80efea0216fd549e02
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2015-04-29 16:00:10 +0200

    [ex1] created separate remove-stopwords method

--- ex1/test.py
+++ ex1/test.py
@@ -47,20 +47,25 @@ def main():
 	for char in stopmarks:
 		text = text.replace(char, " ")
 	
+
+	
 	# convert to array
 	words = text.split()
-
 	print("total words:", len(words))
-	
+
+	# remove stopwords
+	if(read_stopwords):
+		words = remove_stopwords(words)
+
 	# sort
-	unique_word_tuples = create_word_tuples(words)
+	unique_word_tuples = create_tuple_list(words)
 	
 	print("unique words:", len(unique_word_tuples))
 
 	# print top 30
-	tuple_list_print(unique_word_tuples, top_words)
+	print_tuple_list(unique_word_tuples, top_words)
 	
-	tuple_list_write(unique_word_tuples, top_words, file_listoutput_file)
+	write_tuple_list(unique_word_tuples, top_words, file_listoutput_file)
 	sys.exit()
 	return
 
@@ -71,8 +76,21 @@ def txt_read(input):
 		buff = buff + line
 	return buff	
 
+### REMOVES STOPWORDS FROM GIVEN LIST
+def remove_stopwords(words):
+	stopwords = file_stopwords.read()
+		
+	rem_counter = 0
+	for stopword in stopwords.split():
+		while stopword in words:
+			rem_counter += 1
+			words.remove(stopword)
+		
+	print("removed stopwords:", rem_counter)
+	return words
+	
 ### PRINTS THE FIRST i ITEMS
-def tuple_list_print(tuple_list, top_words):
+def print_tuple_list(tuple_list, top_words):
 	length = len(tuple_list)
 	print()
 	print(top_words, "most used words:")
@@ -84,7 +102,7 @@ def tuple_list_print(tuple_list, top_words):
 	return
 	
 ### WRITES THE TUPLE LIST TO THE output_file FILE
-def tuple_list_write(tuple_list, top_words, output_file):
+def write_tuple_list(tuple_list, top_words, output_file):
 	length = len(tuple_list)
 
 	for x in range(0, min(top_words, length)):
@@ -101,43 +119,30 @@ def tuple_list_write(tuple_list, top_words, output_file):
 	print("file write finished")
 	return
 
-### COUNTS UNIQUE WORDS WITHOUT STOPWORDS IF SET AND RETURNS A LIST OF TUPLES
-def create_word_tuples(words):
-	unique_words = []
-	unique_words_counter = []
+### COUNTS UNIQUE WORDS AND RETURNS A LIST OF TUPLES
+def create_tuple_list(word_list):
+	unique_items = []
+	item_counter = []
 	
-	stopwords = ""
-	# read if flag set
-	if(read_stopwords):
-		stopwords = file_stopwords.read()
-	
-	rem_stopwords = 0
-
 	# filter words
-	for word in words:
-		if word not in stopwords:
-			if word not in unique_words:
-				unique_words.append(word)
-				unique_words_counter.append(1)
-			else:
-				index = unique_words.index(word)
-				unique_words_counter[index] += 1
+	for item in word_list:
+		if item not in unique_items:
+			unique_items.append(item)
+			item_counter.append(1)
 		else:
-				rem_stopwords += 1
-	
-	# print
-	print("removed stopwords:", rem_stopwords)
+			index = unique_items.index(item)
+			item_counter[index] += 1
 
 	# create tuple words
-	tuple_words = []
-	for i in range(len(unique_words)):
-		tuple_words.append((unique_words[i], unique_words_counter[i]))
+	tuple_items = []
+	for i in range(len(unique_items)):
+		tuple_items.append((unique_items[i], item_counter[i]))
 	
 	# sort words
-	tuple_words = sorted(tuple_words, key=lambda x: x[1], reverse=True)
+	tuple_items = sorted(tuple_items, key=lambda x: x[1], reverse=True)
 		
 	# return sorted tuple words (word w, count(w))
-	return tuple_words
+	return tuple_items
 	
 ## call main
 main()
