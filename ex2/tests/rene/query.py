34e81a585468a85879f06bca0cd3f1f67e085993
Rwilmes <rene.wilmes@stud.tu-darmstadt.de>
2015-05-23 14:39:31 +0200

    [ex2] added literal-string flag in bing-script

--- ex2/tests/rene/query.py
+++ ex2/tests/rene/query.py
@@ -5,15 +5,20 @@ import sys
 
 output_file = "out.txt"
 
+EXACT = True
+
 """
 MAIN FUNCTION
 """
 def main(argv):
 	query = argv[1]
-	
+
+	if(EXACT):
+		query = '"' + query + '"'
 	print("Query:", query)
 	
 	bing = PyBingSearch()
+	
 	bing.search_all(query, 1000)
 	
 	print("-----------------------")
@@ -25,6 +30,7 @@ def main(argv):
 		output.write(url + "\n")
 		
 	print("writing finished")
+
 	sys.exit()
 	
 	
